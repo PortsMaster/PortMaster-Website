980e0b6c0248670de5dd85d163ae8e17477f24bf
unknown <christopher.roelofs@gmail.com>
2023-10-09 07:24:48 -0400

    fix download url

--- docs/detail.js
+++ docs/detail.js
@@ -44,7 +44,7 @@ function displayCardDetails(data) {
 
 
     const downloadElement = document.getElementById("download");
-    downloadElement.setAttribute("onclick", "window.location.href='" + data.source.download_url + "';");
+    downloadElement.setAttribute("onclick", "window.location.href='" + data.source.url + "';");
 
     const markdownElement = document.getElementById("markdown");
     markdownElement.setAttribute("src", "https://raw.githubusercontent.com/PortsMaster/PortMaster-Website/main/markdown/" + data.name.toLowerCase().replace("zip", "md"));
