16d0dead8aa90173c993c7f5e97813b243710edf
GitHub Action <action@github.com>
2025-04-05 12:43:46 +0000

    updated website content

--- docs/js/port-json.js
+++ docs/js/port-json.js
@@ -85,11 +85,13 @@ function addStoreLink() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
 
   // Add some text to the new cells:
   cell1.innerHTML = '<div><input></div>';
   cell2.innerHTML = '<div><input></div>';
-  cell3.innerHTML = '<input type="button" class="btn btn-danger" value="Delete" onclick="deleteRow(this)"/>';
+  cell3.innerHTML = '<div><input></div>';
+  cell4.innerHTML = '<input type="button" class="btn btn-danger" value="Delete" onclick="deleteRow(this)"/>';
 }
 
 async function populatePorterList() {
@@ -178,9 +180,11 @@ function getFormValues() {
   for (var i = 1, row; row = table.rows[i]; i++) {
     var store = {};
     var name = row.cells[0].getElementsByTagName('input')[0].value;
-    var url = row.cells[1].getElementsByTagName('input')[0].value;
+    var gameurl = row.cells[1].getElementsByTagName('input')[0].value;
+    var developerurl = row.cells[2].getElementsByTagName('input')[0].value;
     store["name"] = name;
-    store["url"] = url;
+    store["gameurl"] = gameurl;
+    store["developerurl"] = developerurl;
     storeLinks.push(store);
   }
   portJson.attr.store = storeLinks;
